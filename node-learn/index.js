45cc31d7ac8d77e51ccc39f7b7e72143da7ac142
CodeWithAbdulRehman <abdulrehmanCScoder304@gmail.com>
2024-09-26 21:29:42 +0500

    Update index.js

--- node-learn/index.js
+++ node-learn/index.js
@@ -1,6 +1,15 @@
 const http = require("node:http");
 
 const server = http.createServer((request, response) => {
+  response.writeHead(200, {
+    "content-type: text/plain"
+  })
+  response.write("String! ", "utf-8", () => {
+    console.log("Response Added to body!");
+  }
+  response.end("Thanks for visiting");
+  console.log(response.writableEnded);
+  console.log(resppnse.writableFinished);
 })
 
 server.listen(7500, () => {
